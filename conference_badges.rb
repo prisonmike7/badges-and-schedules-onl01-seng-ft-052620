c7abc00112e1f88dae0929d667aea42923f03acb
Michael Harris <mwharris7@gmail.com>
2020-05-28 06:53:56 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,6 @@
-# Write your code here.
+
+def batch_badge_creator(array_names)
+  new_batch_array = []
+  array_names.each_with_index { |name, index| new_batch_array << "Hello, #{name}! You'll be assigned to room #{index}!"  }
+  return new_batch_array
+end
