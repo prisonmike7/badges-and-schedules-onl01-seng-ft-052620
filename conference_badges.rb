0498924acfd6fce65e99fc23d5e60e0bdd1b1ce5
Michael Harris <mwharris7@gmail.com>
2020-05-28 07:06:14 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -18,4 +18,8 @@ end
 def printer(array_names)
   name_badge_array = batch_badge_creator(array_names)
   room_assignment_array = assign_rooms(array_names)
+  array_names.each_with_index do |name, index|
+    puts name_badge_array[index]
+    puts room_assignment_array[index]
+  end
 end
